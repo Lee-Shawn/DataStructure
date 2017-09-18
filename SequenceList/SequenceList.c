eef634f16dba74b604e6609724ef1ed51c78f9d6
laughing <425984393@qq.com>
2017-09-18 21:20:40 +0800

    insert and delete

--- SequenceList/SequenceList.c
+++ SequenceList/SequenceList.c
@@ -30,3 +30,57 @@ Position findLocation(List L, ElementType x)
     else
         return i;
 }
+
+/**
+* 插入元素
+*/
+bool insertElement(List L, ElementType x, Position p)
+{
+    int i;
+
+    if (L->last == MAXSIZE-1)
+    {
+        printf("线性表已满！\n");
+        return false;
+    }
+    if (p < 1 || p > L->last+1)
+    {
+        printf("插入的位置不合法！\n");
+        return false;
+    }
+
+    // 从最后一个元素开始往后移一位，直到要插入的位置
+    for (i = L->last; i > p; i--)
+    {
+        L->data[i+1] = L->data[i];
+    }
+    // 赋值给新插入的位置
+    L.data[p] = x;
+    // 顺序表指向新的最后的元素，表长加一
+    L.last++;
+
+    return true;
+}
+
+/**
+* 删除元素
+*/
+bool deleteElement(List L, Position p)
+{
+    int i;
+
+    if (p < 0 || p > L->last)
+    {
+        printf("要删除的位置%d不存在\n", p);
+        return false;
+    }
+
+    // 把p后面的元素向前移动
+    for (i = p+1; i < L->last; i++)
+    {
+        L.data[i-1] = L.data[i];
+    }
+    L->last--;
+
+    return true;
+}
