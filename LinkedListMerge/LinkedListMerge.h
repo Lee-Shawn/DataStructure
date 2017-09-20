c65a836580ef1af17438dfe4a6c4d5ca7ca2e6e9
laughing <425984393@qq.com>
2017-09-20 22:51:29 +0800

    linkedlist merge

---
+++ LinkedListMerge/LinkedListMerge.h
@@ -0,0 +1,13 @@
+#ifndef _LinkedListMerge_H_
+
+typedef int ElementType;
+typedef struct Node *List;
+
+
+
+#endif // _LinkedListMerge_H_
+
+struct Node {
+    ElementType data;
+    List next;
+};
---
+++ LinkedListMerge/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    List L = createList();
+    return 0;
+}
