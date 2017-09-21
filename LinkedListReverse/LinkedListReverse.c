cb7b948650e3f8003b29d8aa773c47e0e851b070
laughing <425984393@qq.com>
2017-09-21 23:10:24 +0800

    finish

--- LinkedListReverse/LinkedListReverse.c
+++ LinkedListReverse/LinkedListReverse.c
@@ -4,15 +4,77 @@
 
 List createList()
 {
+    int i, length, value;
 
+    List pHead = (List)malloc(sizeof(struct Node));
+    pHead->next = NULL;
+    List pTail = pHead;
+
+    if (pHead == NULL)
+    {
+        printf("分配结点失败！\n");
+        exit(-1);
+    }
+
+    printf("输入链表的长度：\n");
+    scanf("%d", &length);
+    printf("输入链表的值：\n");
+
+    for (i = 0; i < length; i++)
+    {
+        List pNode = (List)malloc(sizeof(struct Node));
+
+        if (pNode == NULL)
+        {
+            printf("分配结点失败！\n");
+            exit(-1);
+        }
+
+        scanf("%d", &value);
+        pNode->data = value;
+        pTail->next = pNode;
+        pNode->next = NULL;
+        pTail = pNode;
+    }
+
+    return pHead;
 }
 
 List reverseList(List L)
 {
+    List s;
+    List p = L->next;
+    L->next = NULL;
 
+    if (p == NULL)
+        return NULL;
+
+    while (p)
+    {
+        s = p->next;
+        p->next = L->next;
+        L->next = p;
+        p = s;
+    }
+
+    return L;
 }
 
 void printList(List L)
 {
+    List p = L->next;
 
+    if (p == NULL)
+    {
+        printf("链表为空！\n");
+        exit(0);
+    }
+
+    while (p)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+
+    printf("\n");
 }
--- LinkedListReverse/main.c
+++ LinkedListReverse/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "LinkedListReverse.h"
 
 int main()
 {
     List L1, L2;
     L1 = createList();
+    printf("逆置前的链表值为：\n");
+    printList(L1);
     L2 = reverseList(L1);
+    printf("逆置后的链表值为：\n");
     printList(L2);
 
     return 0;
