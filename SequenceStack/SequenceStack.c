2047810bbf6291e48283b884efcb261e97d8c4a6
laughing <425984393@qq.com>
2017-09-24 23:20:25 +0800

    update

--- SequenceStack/SequenceStack.c
+++ SequenceStack/SequenceStack.c
@@ -2,27 +2,55 @@
 #include <stdlib.h>
 #include "SequenceStack.h"
 
-Stack createStack()
+Stack createStack(int size)
 {
+    Stack s = (Stack)malloc(sizeof(struct SNode));
+    s->data = (ElementType *)malloc(size * sizeof(ElementType));
+    s->top = -1;
 
+    return s;
 }
 
-void push(Stack s, ElementType x)
+int push(Stack s, ElementType x)
 {
-
+    if (isFull(s))
+    {
+        printf("栈已满！\n");
+        return 0;
+    }
+    else
+    {
+        s->data[++(s->top)] = x;
+        return 1;
+    }
 }
 
-void pop(Stack s)
+int pop(Stack s)
 {
-
+    if (isEmpty(s))
+    {
+        printf("栈为空！\n");
+        return 0;
+    }
+    else
+    {
+        s->data[(s->top)--];
+        return 1;
+    }
 }
 
 int isFull(Stack s)
 {
-
+    if (s->top == MaxSize-1)
+        return 1;
+    else
+        return 0;
 }
 
 int isEmpty(Stack s)
 {
-
+    if (s->top == -1)
+        return 1;
+    else
+        return 0;
 }
--- SequenceStack/SequenceStack.h
+++ SequenceStack/SequenceStack.h
@@ -4,6 +4,11 @@
 typedef int ElementType;
 typedef struct SNode *Stack;
 
+Stack createStack(int Size);
+int push(Stack s, ElementType x);
+int pop(Stack s);
+int isFull(Stack s);
+int isEmpty(Stack s);
 
 
 #endif // _SequenceStack_H_
