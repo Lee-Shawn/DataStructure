cabdb6361dc35e15fa5674283d9272c9a9507a54
laughing <425984393@qq.com>
2017-09-24 23:00:09 +0800

    sequence stack

---
+++ SequenceStack/SequenceStack.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SequenceStack.h"
+
+Stack createStack()
+{
+
+}
+
+void push(Stack s, ElementType x)
+{
+
+}
+
+void pop(Stack s)
+{
+
+}
+
+int isFull(Stack s)
+{
+
+}
+
+int isEmpty(Stack s)
+{
+
+}
---
+++ SequenceStack/SequenceStack.h
@@ -0,0 +1,14 @@
+#ifndef _SequenceStack_H_
+
+#define MaxSize 10
+typedef int ElementType;
+typedef struct SNode *Stack;
+
+
+
+#endif // _SequenceStack_H_
+
+struct SNode {
+    ElementType data;
+    int top;
+};
