c9bb5a1ad82ea7b8319edaaf80dd2e7b87eafdbe
laughing <425984393@qq.com>
2017-10-04 23:34:30 +0800

    queue

--- SequenceQueue/SequenceQueue.h
+++ SequenceQueue/SequenceQueue.h
@@ -1,7 +1,21 @@
 #ifndef _SequenceQueue_H_
 
 
+#define MaxSize 10
+typedef int ElementType;
+typedef struct QNode *Queue;
+
+
+void initQueue(Queue Q);
+int isEmpty(Queue Q);
+int isFull(Queue Q);
+int enQueue(Queue Q, ElementType x);
+int deQueue(Queue Q);
 
 #endif // _SequenceQueue_H_
 
+struct QNode {
+    ElementType data[MaxSize];
+    int front, rear;
+};
 
