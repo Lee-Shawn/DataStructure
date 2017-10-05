5ab4a2eb11ae83a637b6bee3fb43d2e6087e7d8c
laughing <425984393@qq.com>
2017-10-05 23:18:42 +0800

    update

--- SequenceQueue/main.c
+++ SequenceQueue/main.c
@@ -4,9 +4,14 @@
 
 int main()
 {
-    Queue Q = initQueue(5);
-    enQueue(Q, 66);
-    enQueue(Q, 88);
+    int N, i, num;
+    scanf("%d", &N);
+    Queue Q = initQueue(N);
+    for (i = 0; i < ; i++)
+    {
+        scanf("%d", &num);
+        enQueue(Q, num);
+    }
     printf("出队的元素是：%d", deQueue(Q));
     return 0;
 }
