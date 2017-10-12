9d9dab71b8fc47037c93a8232168fef2dd9fbece
laughing <425984393@qq.com>
2017-10-12 23:42:12 +0800

    binarytree

--- BinTreeInOrder/BinTree.h
+++ BinTreeInOrder/BinTree.h
@@ -1,1 +1,12 @@
+#ifndef _BinTree_H_
 
+
+typedef struct BTNode *BTree;
+
+#endif // _BinTree_H_
+
+
+struct BTNode {
+    ElementType data;
+    BTree left, right;
+};
--- BinTreeInOrder/Stack.h
+++ BinTreeInOrder/Stack.h
@@ -1,1 +1,13 @@
+#ifndef _Stack_H_
 
+typedef int ElementType;
+typedef struct SNode *Stack;
+
+#endif // _Stack_H_
+
+
+struct SNode {
+    ElementType *data;
+    int top;
+    int MaxSize;
+};
