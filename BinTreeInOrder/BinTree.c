963e2cf99d84f6bd72ac18120b9e7c75216c7060
laughing <425984393@qq.com>
2017-10-15 23:31:38 +0800

    create tree

--- BinTreeInOrder/BinTree.c
+++ BinTreeInOrder/BinTree.c
@@ -2,3 +2,24 @@
 #include <stdlib.h>
 #include "Stack.h"
 #include "BinTree.h"
+
+BTree createBTree()
+{
+    BTree BT;
+    ElementType data;
+    scanf("%d", &data);
+
+    if (BT == 0)
+    {
+        BT == NULL;
+    }
+    else
+    {
+        BT = (BTree)malloc(sizeof(struct BTNode));
+        BT->data = data;
+        BT->left = createBTree();
+        BT->right = createBTree();
+    }
+
+    return BT;
+}
