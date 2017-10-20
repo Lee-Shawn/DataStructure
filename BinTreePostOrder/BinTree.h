640dff8cf59b952863e7b384239ea0b135c6d132
laughing <425984393@qq.com>
2017-10-20 23:41:55 +0800

    head file

--- BinTreePostOrder/BinTree.h
+++ BinTreePostOrder/BinTree.h
@@ -1,1 +1,11 @@
+#ifndef _BinTree_H_
 
+typedef struct TNode *BTree;
+
+
+#endif // _BinTree_H_
+
+struct TNode {
+    ElementType data;
+    BTree left, right;
+};
--- BinTreePostOrder/Stack.h
+++ BinTreePostOrder/Stack.h
@@ -1,1 +1,14 @@
+#ifndef _Stack_H_
 
+typedef int ElementType;
+typedef struct SNode *Stack;
+
+
+#endif // _Stack_H_
+
+
+struct SNode {
+    ElementType data;
+    int top;
+    int maxSize;
+};
